c2e05088e5f41728cdba4b4b0d0f5714f6ab86f4
Tom Nolan <tsnolan@outlook.com>
2025-03-01 18:46:25 -0500

    feat: calculate final dimensions

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,18 @@ const FRAME_WIDTH = 1.5;
 const WIDTH = 48;
 const HEIGHT = 24;
 const MATTING = 4;
+const NOTCH_SIZE = 0.5;
+
+function calculateOverallDimensions(
+  width: number,
+  height: number,
+  matting: number,
+  frame: number
+) {
+  const finalWidth = width + matting * 2 + frame * 2 - NOTCH_SIZE * 2;
+  const finalHeight = height + matting * 2 + frame * 2 - NOTCH_SIZE * 2;
+  return `${finalWidth}" x ${finalHeight}"`;
+}
 
 function App() {
   const [width, setWidth] = useState(WIDTH);
@@ -15,7 +27,7 @@ function App() {
   const [frame, setFrame] = useState(FRAME_WIDTH);
 
   return (
-    <div className="p-8 container mx-auto max-w-3xl space-y-8 min-h-screen h-screen max-h-screen overflow-hidden flex flex-col">
+    <div className="p-4 sm:p-8 container mx-auto max-w-3xl space-y-8 min-h-screen h-screen max-h-screen overflow-hidden flex flex-col">
       <h1 className="mx-auto text-2xl font-bold text-orange-900">Framer</h1>
       <div className="sm:flex sm:justify-around grid grid-cols-2 gap-8">
         <div className="inline-flex flex-col items-center space-y-2 col-span-2">
@@ -66,6 +78,10 @@ function App() {
           matting={matting}
           frame={frame}
         />
+      </div>
+      <div className="text-center">
+        Overall Dimensions:{" "}
+        {calculateOverallDimensions(width, height, matting, frame)}
       </div>
     </div>
   );
